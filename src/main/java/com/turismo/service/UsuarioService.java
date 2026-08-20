9a10841f3ac2085984dc49bdc480258c24d885e8
CarMorv <carlosmorenovi2000@gmail.com>
2026-08-19 20:31:30 -0500

    feat(service): crear capa de servicios com.turismo.service con UsuarioService (Fase 4)

---
+++ src/main/java/com/turismo/service/UsuarioService.java
@@ -0,0 +1,26 @@
+﻿package com.turismo.service;
+
+import com.turismo.dao.DAOFactory;
+import com.turismo.interfaces.UsuarioInterface;
+import com.turismo.modelo.Usuario;
+
+public class UsuarioService {
+
+    private UsuarioInterface usuarioDao = DAOFactory.getDaoFactory(DAOFactory.MYSQL).getUsuario();
+
+    public Usuario login(String email, String password) {
+        return usuarioDao.login(email, password);
+    }
+
+    public boolean registrar(Usuario usuario) {
+        return usuarioDao.registrar(usuario);
+    }
+
+    public boolean actualizar(Usuario usuario) {
+        return usuarioDao.actualizar(usuario);
+    }
+
+    public boolean actualizarConPassword(Usuario usuario) {
+        return usuarioDao.actualizarConPassword(usuario);
+    }
+}
